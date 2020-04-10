942b0567250a45738ceca79b60930521b439d635
rogpe <prog_x@hotmail.com>
2020-04-10 11:49:31 -0300

    development page front with EJS

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const path = require('path')
+
+app.use(express.static(__dirname +'/public'))
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.send('ALOHAS!!!!')
+    res.render('home')
 })
 
 app.listen(port, () => console.log('running...'))
